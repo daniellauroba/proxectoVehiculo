fecd4f5f35c3c57adb37b10bbbba2163497652dd
DAM1_Alu08 <DAM1_Alu08@equip077238.xunta.local>
2021-05-11 16:26:54 +0200

    en proceso

---
+++ gestionDeVehiculos/src/main/java/com/mycompany/gestiondevehiculos/xestionVehiculos.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.gestiondevehiculos;
+
+/**
+ *
+ * @author DAM1_Alu08
+ */
+public class xestionVehiculos extends vehiculo{
+     public static void main(String[] args) {
+        do {
+            System.out.println("1. nombreVehiculo");
+            System.out.println("2. matricula");
+            System.out.println("3. km");
+            System.out.println("4. actualizacion Km");
+            System.out.println("5. antiguedade");
+            System.out.println("6. propietario");
+            System.out.println("7.descripcion");
+            System.out.println("8. salir");
+
+            int opcion;
+            switch (opcion = 0) {
+                case 1:
+                    
+                    break;
+            }
+        }
+        
+    
+    
+    }
+}
